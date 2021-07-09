527d24553d1a4c1bf638bbf5a269e2301387125f
francesco-rainone <francesco.rainone@adevinta.com>
2021-07-09 20:38:28 +0200

    remove travis variables

--- scripts/common.sh
+++ scripts/common.sh
@@ -13,14 +13,8 @@ TEST_CONTAINER_NAME=test_ipytv
 LINT_CONTAINER_NAME=lint_ipytv
 TEST_IN_CONTAINER=/usr/bin/runtest
 LINT_IN_CONTAINER=/usr/bin/runlint
-VERSION=${TRAVIS_TAG}
-if [[ -n "${CIRCLE_TAG}" ]]; then
-    VERSION="${CIRCLE_TAG}"
-fi
-TEST_VERSION="0.0.${TRAVIS_BUILD_NUMBER}"
-if [[ -n "${CIRCLE_BUILD_NUM}" ]]; then
-    TEST_VERSION="0.0.${CIRCLE_BUILD_NUM}"
-fi
+VERSION="${CIRCLE_TAG}"
+TEST_VERSION="0.0.${CIRCLE_BUILD_NUM}"
 
 # Functions --------------------------------------------------------------------
 function delete_container() {
